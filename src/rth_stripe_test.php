27a9b120fb569f7fdd5d5dc5f6a11d65ebbea24e
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-02-28 17:56:30 +0100

    chore: adding stripe/stripe-php lib to project

---
+++ src/rth_stripe_test.php
@@ -0,0 +1,17 @@
+<?php
+
+use Stripe\StripeClient;
+
+include 'includes/application_top.php';
+
+if (rth_is_module_disabled('MODULE_RTH_STRIPE')) {
+    return;
+}
+
+$stripe = new StripeClient('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
+$customer = $stripe->customers->create([
+    'description' => 'example customer',
+    'email' => 'email@example.com',
+    'payment_method' => 'pm_card_visa',
+]);
+echo $customer;
