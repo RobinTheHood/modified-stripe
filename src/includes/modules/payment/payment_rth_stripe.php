a858a1c5f162183734e67e3c16ba66ce2665bfdf
Jay Trees <github.jay@grandel.anonaddy.me>
2023-03-01 09:21:31 +0100

    docs: add php docblock (#15)

--- src/includes/modules/payment/payment_rth_stripe.php
+++ src/includes/modules/payment/payment_rth_stripe.php
@@ -3,8 +3,17 @@
 use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 /**
- * You can find informations about payment classes and development at:
+ * Stripe integration for modified
+ *
+ * You can find informations about system classes and development at:
  * https://docs.module-loader.de
+ *
+ * @author  Robin Wieschendorf <mail@robinwieschendorf.de>
+ * @author  Jay Trees <stripe@grandels.email>
+ * @link    https://github.com/RobinTheHood/modified-stripe/
+ *
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+ * @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
  */
 class payment_rth_stripe extends StdModule
 {
