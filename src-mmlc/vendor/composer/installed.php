9573725316f1730a036ad0d9bb4c9da4080636bf
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-04-20 10:39:50 +0200

    refactor: move code to src-mmlc

---
+++ src-mmlc/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'robinthehood/stripe',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '27a9b120fb569f7fdd5d5dc5f6a11d65ebbea24e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'robinthehood/stripe' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '27a9b120fb569f7fdd5d5dc5f6a11d65ebbea24e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.11.0',
+            'version' => '10.11.0.0',
+            'reference' => '899f50664d47ddac4f9c1ce871a4aca402f963a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
