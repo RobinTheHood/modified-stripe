1ce92cd8b3f84e8a20bfe32dbbf72c2ca57b2c8f
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-04-20 10:46:37 +0200

    feat: adding strip lib (#17)
    * chore: adding stripe/stripe-php lib to project
    * refactor: move code to src-mmlc

---
+++ src-mmlc/Classes/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Stripe integration for modified
+ *
+ * You can find informations about system classes and development at:
+ * https://docs.module-loader.de
+ *
+ * @author  Robin Wieschendorf <mail@robinwieschendorf.de>
+ * @author  Jay Trees <stripe@grandels.email>
+ * @link    https://github.com/RobinTheHood/modified-stripe/
+ */
