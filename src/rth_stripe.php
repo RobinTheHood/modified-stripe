31aa51dd8aafdff0b30691bff91ef5fdfee1c21b
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-05-24 13:23:27 +0200

    feat: add controller and controller entry point file

---
+++ src/rth_stripe.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Stripe integration for modified
+ *
+ * You can find informations about system classes and development at:
+ * https://docs.module-loader.de
+ *
+ * @author  Robin Wieschendorf <mail@robinwieschendorf.de>
+ * @author  Jay Trees <stripe@grandels.email>
+ * @link    https://github.com/RobinTheHood/modified-stripe/
+ *
+ */
+
+use RobinTheHood\Stripe\Classes\Controller;
+
+include 'includes/application_top.php';
+
+$rthDevMode = true;
+
+if ($rthDevMode === true) {
+    restore_error_handler();
+    restore_exception_handler();
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL ^ E_NOTICE);
+}
+
+/**
+ * The function rth_is_module_disabled() is part of the StdModule. It is a helper to wrtie shorter code to check, if a
+ * module is installed or not.
+ * 
+ * @link //TODO Documentation link to StdModule
+ * @link https://github.com/RobinTheHood/modified-std-module
+ */
+if (rth_is_module_disabled('MODULE_RTH_STRIPE')) {
+    return;
+}
+
+$controller = new Controller();
+$controller->invoke();
