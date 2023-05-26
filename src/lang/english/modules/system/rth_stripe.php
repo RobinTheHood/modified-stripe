1ef7990338721df830ca15e1df2b266299634839
grandeljay <github.jay@grandel.anonaddy.me>
2023-05-26 13:24:50 +0200

    fix(localisation): #28 invalid constant prefix

--- src/lang/english/modules/system/rth_stripe.php
+++ src/lang/english/modules/system/rth_stripe.php
@@ -1,8 +1,8 @@
 <?php
 
-$moduleType = 'MODULE';
-$moduleName = 'RTH_STRIPE';
-$prefix     = $moduleType . '_' . $moduleName  . '_';
+use RobinTheHood\Stripe\Classes\Constants;
+
+$prefix = Constants::MODULE_SYSTEM_NAME . '_';
 
 define($prefix . 'TITLE', 'Stripe Paymentmodule © by <a href="https://github.com/RobinTheHood/modified-stripe" target="_blank" style="font-weight: bold">RobinTheHood, grandeljay</a>');
 define($prefix . 'LONG_DESCRIPTION', 'A modified-shop module that allows payments via Stripe.');
