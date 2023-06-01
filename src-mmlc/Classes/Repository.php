0945247e1af0dd4c69df0c544c824efde1ab8adc
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-06-01 23:41:18 +0200

    feat: throw an Exception if an SQL error occurs

--- src-mmlc/Classes/Repository.php
+++ src-mmlc/Classes/Repository.php
@@ -15,27 +15,38 @@ declare(strict_types=1);
 
 namespace RobinTheHood\Stripe\Classes;
 
+use Exception;
+
 /**
  * In this class we outsource all queries to the database. So we only need SQL in this file.
  */
 class Repository
 {
+    public function test()
+    {
+        $sql = "SELECT x";
+
+        $this->query($sql);
+    }
+
     public function createRthStripePhpSession()
     {
-        xtc_db_query("CREATE TABLE `rth_stripe_php_session` (
+        $sql = "CREATE TABLE `rth_stripe_php_session` (
             `id` varchar(32) NOT NULL,
             `created` datetime DEFAULT NULL,
             `data` longtext DEFAULT NULL,
             PRIMARY KEY (`id`)
           );
-        ");
+        ";
+
+        $this->query($sql);
     }
 
     public function getRthStripePhpSessionById(string $id)
     {
         $sql = "SELECT * FROM rth_stripe_php_session WHERE id='$id'";
 
-        $query = xtc_db_query($sql);
+        $query = $this->query($sql);
 
         $row = xtc_db_fetch_array($query);
         if (!isset($row['id'])) {
@@ -53,13 +64,13 @@ class Repository
             '$id', '$data', NOW()
         )";
 
-        xtc_db_query($sql);
+        $this->query($sql);
     }
 
     public function updateOrderStatus(int $orderId, int $statusId): void
     {
         $sql = "UPDATE `orders` SET `orders_status` = '$statusId', `last_modified` = NOW() WHERE orders_id = '$orderId'";
-        xtc_db_query($sql);
+        $this->query($sql);
     }
 
     public function insertOrderStatusHistory(int $orderId, int $statusId, string $comment = '')
@@ -69,6 +80,18 @@ class Repository
         ) VALUES (
             '$orderId', '$statusId', NOW(), '0', '$comment', '0'
         )";
-        xtc_db_query($sql);
+
+        $this->query($sql);
+    }
+
+    private function query(string $sql)
+    {
+        $query = xtc_db_query($sql);
+
+        if (!$query) {
+            throw new Exception("Error in Repository SQL Query, you can find more infos in logs - $sql");
+        }
+
+        return $query;
     }
 }
