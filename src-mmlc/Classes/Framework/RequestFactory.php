49357d4972f9d2bdc446d6a2101d38e5a3c252e6
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-06-05 10:10:30 +0200

    feat: add class RequestFactory

---
+++ src-mmlc/Classes/Framework/RequestFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Stripe integration for modified
+ *
+ * You can find informations about system classes and development at:
+ * https://docs.module-loader.de
+ *
+ * @author  Robin Wieschendorf <mail@robinwieschendorf.de>
+ * @author  Jay Trees <stripe@grandels.email>
+ * @link    https://github.com/RobinTheHood/modified-stripe/
+ */
+
+declare(strict_types=1);
+
+namespace RobinTheHood\Stripe\Classes\Framework;
+
+class RequestFactory
+{
+    public static function createFromGlobals(): Request
+    {
+        return new Request($_GET, $_POST);
+    }
+}
