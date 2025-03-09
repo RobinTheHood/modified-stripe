ea61f652c3d38a8e937c236a28dd35cfb625f673
Robin Wieschendorf <mail@robinwieschendorf.de>
2025-03-09 19:29:36 +0100

    wip: add StripeConfig class for improved configuration management in Stripe integration

---
+++ src-mmlc/Classes/Config/StripeConfig.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Stripe integration for modified
+ *
+ * You can find informations about system classes and development at:
+ * https://docs.module-loader.de
+ *
+ * @author  Robin Wieschendorf <mail@robinwieschendorf.de>
+ * @author  Jay Trees <stripe@grandels.email>
+ * @link    https://github.com/RobinTheHood/modified-stripe/
+ */
+
+declare(strict_types=1);
+
+namespace RobinTheHood\Stripe\Classes\Config;
+
+use Exception;
+use RobinTheHood\ModifiedStdModule\Classes\Configuration;
+use RobinTheHood\Stripe\Classes\Framework\DIContainer;
+use RobinTheHood\Stripe\Classes\Repository\ConfigurationRepository;
+
+/**
+ * This class makes it easy to access the modified user configuration of the Stripe module. Unlike the pure
+ * Configuration class, which creates many magic attributes, this class helps the IDE to show us autocompletion.
+ *
+ * @link https://github.com/RobinTheHood/modified-std-module#easy-access-with-class-configuration
+ */
+class StripeConfig extends Configuration
+{
+    public function getLiveMode(): bool
+    {
+        try {
+            return 'true' === $this->liveMode ? true : false;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function getApiSandboxSecret(): string
+    {
+        try {
+            return $this->apiSandboxSecret;
+        } catch (Exception $e) {
+            return '';
+        }
+    }
+
+    public function getApiLiveSecret(): string
+    {
+        try {
+            return $this->apiLiveSecret;
+        } catch (Exception $e) {
+            return '';
+        }
+    }
+
+    public function getApiLiveEndpointSecret(): string
+    {
+        try {
+            return $this->apiLiveEndpointSecret;
+        } catch (Exception $e) {
+            return '';
+        }
+    }
+
+    public function getOrderStatusPending(int $default = 0): int
+    {
+        try {
+            return (int) $this->orderStatusPending;
+        } catch (Exception $e) {
+            return $default;
+        }
+    }
+
+    public function getOrderStatusPaid(int $default = 0): int
+    {
+        try {
+            return (int) $this->orderStatusPaid;
+        } catch (Exception $e) {
+            return $default;
+        }
+    }
+
+    public function getOrderStatusAuthorized(int $default = 0): int
+    {
+        try {
+            return (int) $this->orderStatusAuthorized;
+        } catch (Exception $e) {
+            return $default;
+        }
+    }
+
+    public function setWebhookSerect(string $secret): void
+    {
+        $container = new DIContainer();
+
+        // $repo = $container->get(Repository::class);
+        // $repo->updateConfigurationValue('MODULE_PAYMENT_PAYMENT_RTH_STRIPE_API_LIVE_ENDPOINT_SECRET', $secret);
+
+        $configurationRepo = $container->get(ConfigurationRepository::class);
+        $configurationRepo->updateValue('MODULE_PAYMENT_PAYMENT_RTH_STRIPE_API_LIVE_ENDPOINT_SECRET', $secret);
+    }
+
+    public function getManualCapture(): bool
+    {
+        try {
+            return 'true' === $this->manualCapture ? true : false;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    public function getPaymentTitle(): string
+    {
+        try {
+            return $this->paymentTitle;
+        } catch (Exception $e) {
+            return '';
+        }
+    }
+
+    public function getPaymentDescription(): string
+    {
+        try {
+            return $this->paymentDesc;
+        } catch (Exception $e) {
+            return '';
+        }
+    }
+}
