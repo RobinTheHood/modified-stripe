274c857bf3d01e2244b59c5581b1ab19ebcef928
Robin Wieschendorf <mail@robinwieschendorf.de>
2025-03-14 13:05:11 +0100

    feat: link payment without event (#70)

--- src-mmlc/Classes/Service/SessionService.php
+++ src-mmlc/Classes/Service/SessionService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace RobinTheHood\Stripe\Classes\Service;
 
 use RobinTheHood\Stripe\Classes\Config\StripeConfig;
+use RobinTheHood\Stripe\Classes\Repository\PaymentRepository;
 use RobinTheHood\Stripe\Classes\Storage\PhpSession;
 use Stripe\StripeClient;
 
@@ -13,13 +14,16 @@ class SessionService
     private const RECONSTRUCT_SESSION_TIMEOUT = 60 * 60;
 
     private PhpSession $phpSession;
+    private PaymentRepository $paymentRepository;
     private StripeConfig $stripeConfig;
 
     public function __construct(
         PhpSession $phpSession,
+        PaymentRepository $paymentRepository,
         StripeConfig $stripeConfig
     ) {
         $this->phpSession = $phpSession;
+        $this->paymentRepository = $paymentRepository;
         $this->stripeConfig = $stripeConfig;
     }
 
@@ -34,6 +38,13 @@ class SessionService
 
         $this->phpSession->load($sessionId, self::RECONSTRUCT_SESSION_TIMEOUT);
         $_SESSION['rth_stripe_status'] = 'success';
+
+        $orderId = $this->phpSession->getOrder()->getId();
+        $paymentIntentId = $stripeCheckoutSession->payment_intent;
+
+        if ($paymentIntentId && $orderId) {
+            $this->paymentRepository->add($orderId, $paymentIntentId);
+        }
 
         return $sessionId;
     }
