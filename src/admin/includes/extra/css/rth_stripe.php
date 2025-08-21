15beb70bd701affdcaa75d7c0b2e58187f3ba894
Robin Wieschendorf <mail@robinwieschendorf.de>
2025-08-21 09:24:37 +0200

    refactor: remove unnecessary require_once for rth_modified_std_module in stripe integration files

--- src/admin/includes/extra/css/rth_stripe.php
+++ src/admin/includes/extra/css/rth_stripe.php
@@ -13,8 +13,6 @@
 
 namespace RobinTheHood\Stripe;
 
-require_once DIR_FS_DOCUMENT_ROOT . '/includes/extra/functions/rth_modified_std_module.php';
-
 if (rth_is_module_disabled('MODULE_PAYMENT_PAYMENT_RTH_STRIPE')) {
     return;
 }
